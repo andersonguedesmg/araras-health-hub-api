4d1c9e0f0c2956f2391e3e6567092c7999a15367
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-01-31 16:44:13 -0300

    feat(destination): Ajuste na Validação

--- Dtos/Destination/UpdateDestinationRequestDto.cs
+++ Dtos/Destination/UpdateDestinationRequestDto.cs
@@ -26,7 +26,7 @@ namespace araras_health_hub_api.Dtos.Destination
         [MaxLength(280, ErrorMessage = "O Bairro não pode ter mais de 280 caracteres.")]
         public string Neighborhood { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "A Cidade é obrigatório.")]
+        [Required(ErrorMessage = "A Cidade é obrigatória.")]
         [MinLength(5, ErrorMessage = "A Cidade não pode ter menos de 5 caracteres.")]
         [MaxLength(280, ErrorMessage = "A Cidade não pode ter mais de 280 caracteres.")]
         public string City { get; set; } = string.Empty;
@@ -39,20 +39,20 @@ namespace araras_health_hub_api.Dtos.Destination
         [StringLength(10, ErrorMessage = "O CEP precisa ter 10 caracteres.")]
         public string Cep { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "O E-mail é obrigatório")]
-        [MinLength(5, ErrorMessage = "O E-mail não pode ter menos de 5 caracteres")]
-        [MaxLength(280, ErrorMessage = "O E-mail não pode ter mais de 280 caracteres ")]
+        [Required(ErrorMessage = "O E-mail é obrigatório.")]
+        [MinLength(5, ErrorMessage = "O E-mail não pode ter menos de 5 caracteres.")]
+        [MaxLength(280, ErrorMessage = "O E-mail não pode ter mais de 280 caracteres.")]
         public string Email { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "O Telefone é obrigatório")]
-        [MinLength(14, ErrorMessage = "O Telefone não pode ter menos de 14 caracteres")]
-        [MaxLength(15, ErrorMessage = "O Telefone não pode ter mais de 15 caracteres ")]
+        [Required(ErrorMessage = "O Telefone é obrigatório.")]
+        [MinLength(14, ErrorMessage = "O Telefone não pode ter menos de 14 caracteres.")]
+        [MaxLength(15, ErrorMessage = "O Telefone não pode ter mais de 15 caracteres.")]
         public string Phone { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "A Data de Atualização é obrigatória.")]
         public DateTime UpdatedOn { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O Status é obrigatório.")]
         public bool IsActive { get; set; } = true;
     }
 }
