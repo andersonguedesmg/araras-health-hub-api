085c40015da8e311b4f63e02febe96d794805458
Anderson Guedes <anderson.guedes@outlook.com.br>
2025-12-14 10:05:33 -0300

    feat(order): ajusta FinalizeOrder

--- src/ArarasHealthHub.Application/Features/Orders/Commands/FinalizeOrder/FinalizeOrderCommandHandler.cs
+++ src/ArarasHealthHub.Application/Features/Orders/Commands/FinalizeOrder/FinalizeOrderCommandHandler.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using ArarasHealthHub.Application.Features.Orders.Dtos;
 using ArarasHealthHub.Application.Interfaces.Repositories;
 using ArarasHealthHub.Domain.Enums;
+using ArarasHealthHub.Domain.Identity;
 using ArarasHealthHub.Shared.Core;
 using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 
 namespace ArarasHealthHub.Application.Features.Orders.Commands.FinalizeOrder
 {
@@ -17,18 +20,21 @@ namespace ArarasHealthHub.Application.Features.Orders.Commands.FinalizeOrder
         private readonly IOrderRepository _orderRepo;
         private readonly IEmployeeRepository _employeeRepo;
         private readonly IMapper _mapper;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly IHttpContextAccessor _httpContextAccessor;
 
         public FinalizeOrderCommandHandler(
             IOrderRepository orderRepo,
             IEmployeeRepository employeeRepo,
             IMapper mapper,
-            IHttpContextAccessor httpContextAccessor)
+            IHttpContextAccessor httpContextAccessor,
+            UserManager<ApplicationUser> userManager)
         {
             _orderRepo = orderRepo;
             _employeeRepo = employeeRepo;
             _mapper = mapper;
             _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
         }
 
         public async Task<ApiResponse<OrderDto>> Handle(FinalizeOrderCommand request, CancellationToken cancellationToken)
@@ -51,15 +57,22 @@ namespace ArarasHealthHub.Application.Features.Orders.Commands.FinalizeOrder
                 return new ApiResponse<OrderDto>(StatusCodes.Status404NotFound, ApiMessages.NotFound("Responsável"), false);
             }
 
+            var applicationUser = await _userManager.FindByIdAsync(request.FinalizedByAccountId.ToString());
+
+            if (applicationUser == null)
+            {
+                return new ApiResponse<OrderDto>(StatusCodes.Status404NotFound, ApiMessages.NotFound("Conta"), false);
+            }
+
             var currentUser = _httpContextAccessor.HttpContext?.User;
             var userFacilityClaim = currentUser?.FindFirst("FacilityId");
 
-            if (userFacilityClaim == null || !int.TryParse(userFacilityClaim.Value, out int userFacilityId))
+            if (userFacilityClaim == null)
             {
                 return new ApiResponse<OrderDto>(StatusCodes.Status403Forbidden, ApiMessages.InsufficientPermissions, false);
             }
 
-            if (order.OrderFacilityId != userFacilityId)
+            if (order.OrderFacilityId != applicationUser.FacilityId)
             {
                 return new ApiResponse<OrderDto>(StatusCodes.Status403Forbidden, ApiMessages.OperationRestrictedToFacility, false);
             }
@@ -71,6 +84,7 @@ namespace ArarasHealthHub.Application.Features.Orders.Commands.FinalizeOrder
             order.SetUpdatedOn();
 
             _orderRepo.UpdateWithoutSaving(order);
+            await _orderRepo.SaveAllAsync(cancellationToken);
             var orderDto = _mapper.Map<OrderDto>(order);
 
             return new ApiResponse<OrderDto>(StatusCodes.Status200OK, ApiMessages.OrderSuccessfully("finalizado"), orderDto);
