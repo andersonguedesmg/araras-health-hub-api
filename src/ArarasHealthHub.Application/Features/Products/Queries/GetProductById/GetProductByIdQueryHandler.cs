a426789cf210ca9ef589e18ccc1e63dd1547e808
Anderson Guedes <anderson.guedes@outlook.com.br>
2026-07-18 12:03:09 -0300

    refactor(products): substituir mapeamento do AutoMapper por projeção manual explícita

--- src/ArarasHealthHub.Application/Features/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
+++ src/ArarasHealthHub.Application/Features/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
@@ -8,8 +8,6 @@ using ArarasHealthHub.Application.Interfaces.Repositories;
 using ArarasHealthHub.Shared.Exceptions;
 using ArarasHealthHub.Shared.Results;
 
-using AutoMapper;
-
 using MediatR;
 
 namespace ArarasHealthHub.Application.Features.Products.Queries.GetProductById
@@ -17,28 +15,35 @@ namespace ArarasHealthHub.Application.Features.Products.Queries.GetProductById
     public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Result<ProductResponse>>
     {
         private readonly IProductRepository _productRepository;
-        private readonly IMapper _mapper;
 
-        public GetProductByIdQueryHandler(
-            IProductRepository productRepository,
-            IMapper mapper)
+        public GetProductByIdQueryHandler(IProductRepository productRepository)
         {
             _productRepository = productRepository;
-            _mapper = mapper;
         }
 
         public async Task<Result<ProductResponse>> Handle(
             GetProductByIdQuery request,
             CancellationToken cancellationToken)
         {
-
-            var product = await _productRepository
-                .GetByIdAsync(request.Id, cancellationToken);
+            var product = await _productRepository.GetByIdAsync(request.Id, cancellationToken);
 
             if (product is null)
                 throw new NotFoundException("Produto não foi encontrado.");
 
-            var response = _mapper.Map<ProductResponse>(product);
+            var response = new ProductResponse(
+                product.Id,
+                product.Name,
+                product.Description,
+                product.MainCategoryId,
+                product.MainCategory?.Name ?? string.Empty,
+                product.SubCategoryId,
+                product.SubCategory?.Name ?? string.Empty,
+                product.PackagingTypeId,
+                product.PackagingType?.Name ?? string.Empty,
+                product.CreatedOn,
+                product.UpdatedOn,
+                product.IsActive
+            );
 
             return Result<ProductResponse>.Success(
                 response,
